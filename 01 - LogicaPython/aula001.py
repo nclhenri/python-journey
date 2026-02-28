191eb2076fe188e92c54bd56e1109892ee09b845
nclhenri <nicolashenrisilva@outlook.com>
2026-02-28 17:08:23 -0300

    Conteúdo relacionado ao uso de comentários e DocString em Python

--- 01 - LogicaPython/aula001.py
+++ 01 - LogicaPython/aula001.py
@@ -1,10 +1,12 @@
-# Isso é um comentário em Python
-print("Aula sobre comentários em Python!") # Também posso ter comentário aqui.
-# E aqui!
+# Isso é um comentário em Python. print(123)
+print("Aula sobre comentários em Python!") # Também posso ter comentário aqui. print(123)
+# E aqui! print(123)
 """
 Olá, eu sou uma DocString!
 O interpretador me lê mas não me executa.
+print(123)
 """
 '''
 Também posso ser escrito envolvido por três aspas simples.
+print(123)
 '''
