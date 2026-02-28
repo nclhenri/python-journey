71f4aa7f0fdc128c8ad7515946f86e6777c026cb
nclhenri <nicolashenrisilva@outlook.com>
2026-02-28 19:07:49 -0300

    Estudos sobre as diferentes formas de usar aspas simples e duplas / escape / r

---
+++ 01 - LogicaPython/aula003.py
@@ -0,0 +1,15 @@
+print(1234)
+# Resultado que será impresso na tela: 1234
+print("Nicolas Henrique")
+# Resultado que será impresso na tela: Nicolas Henrique
+print('Nicolas Henrique')
+# Resultado que será impresso na tela: Nicolas Henrique
+print("Nicolas \"Henrique\"")
+# Resultado que será impresso na tela: Nicolas "Henrique" - Escape
+print(r"Nicolas \"Henrique\"")
+# Resultado que será impresso na tela: Nicolas \"Henrique\" - r
+"""
+Para manter o código mais limpo, é recomendável (também) inverter o uso das aspas.
+"""
+print('Nicolas "Henrique"')
+# Resultado que será impresso na tela: Nicolas "Henrique"
