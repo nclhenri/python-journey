11383179e8cefc89a9b945e5f1967b4214435b10
nclhenri <nicolashenrisilva@outlook.com>
2026-02-28 17:35:06 -0300

    Estudos sobre o uso do comando print

---
+++ 01 - LogicaPython/aula002.py
@@ -0,0 +1,27 @@
+print("Estudos sobre o uso de print do Nicolas.")
+# Resultado que será impresso na tela: Estudos sobre o uso de print do Nicolas.
+
+print("Nicolas", "Henrique")
+# Resultado que será impresso na tela: Nicolas Henrique.
+# Padrão do comando o uso da vírgular servir como espaçamento.
+
+print("Nicolas", "Henrique", sep="")
+# Resultado que será impressoa na tela: NicolasHenrique.
+
+print("Nicolas", "Henrique", sep="-")
+# Resultado que será impresso na tela: Nicolas-Henrique.
+
+print("Nicolas", "Henrique", end=".")
+# Resultado que será impresso na tela: Nicolas-Henrique. (Com ponto final)
+
+"""
+Convenções de Quebra de Linha:
+
+- \r\n : CRLF (Carriage Return + Line Feed)
+          Usado principalmente em sistemas Windows.
+
+- \n   : LF (Line Feed)
+          Usado em sistemas Unix/Linux e também no macOS moderno.
+
+"""
+
