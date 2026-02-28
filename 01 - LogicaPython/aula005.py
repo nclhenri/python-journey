0b1f6c24a51fc2770e73f3bd4b5f021cfe5203e2
nclhenri <nicolashenrisilva@outlook.com>
2026-02-28 19:43:26 -0300

    Estudando sobre o tipo de dado booleano e operador lógico igual

---
+++ 01 - LogicaPython/aula005.py
@@ -0,0 +1,13 @@
+"""
+Introdução do operador lógico igual (==), aqui ele verifica se dois dados são iguais ou não
+e a partir desse dado, futuramente, mudar o fluxo do programa para diferentes caminhos.
+"""
+
+print(10 == 10)
+# Resultado que será impresso na tela: True
+
+print(10 == 9)
+# Resultado que será impresso na tela: False
+
+print(type(10 == 10))
+# Resultado que será impresso na tela: <class 'bool'>
