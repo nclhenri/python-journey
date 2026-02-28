380ed55a556c84f8d45a780386023732623271bc
nclhenri <nicolashenrisilva@outlook.com>
2026-02-28 19:28:10 -0300

    Trabalhando com int, float e a classe type

---
+++ 01 - LogicaPython/aula004.py
@@ -0,0 +1,29 @@
+"""
+Int - Números Inteiros
+"""
+print(4)
+print(-4)
+print(0) 
+
+"""
+Float - Números Decimais
+"""
+
+print(4.4, 10.11)
+# Resultado que será impresso na tela: 4.4 10.11
+
+"""
+Type - Tipo do valor recebido
+"""
+
+print(type("Nicolas Henrique"))
+# Resultado que será impresso na tela: <class 'str'>
+
+print(type(4))
+# Resultado que será impresso na tela: <class 'int'>
+
+print(type(4.4))
+# Resultado que será impresso na tela: <class 'float'>
+
+print(type(True))
+# Resultado que será impresso na tela: <class 'bool'>
