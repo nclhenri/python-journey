59119da71286b62f892b926040e1d6b4d6968a94
nclhenri <nicolashenrisilva@outlook.com>
2026-03-02 19:02:48 -0300

    Estudos sobre variáveis

---
+++ 01 - LogicaPython/aula007.py
@@ -0,0 +1,13 @@
+nome_completo = "Nicolas Henrique"
+print(nome_completo)
+# Resultado que será impresso na tela: Nicolas Henrique
+
+soma_dois_mais_dois = 2 + 2
+print(nome_completo ,soma_dois_mais_dois)
+# Resultado que será impresso na tela: Nicolas Henrique 4
+
+nome = "Nicolas"
+idade = 22
+maior_de_idade = idade >= 18
+# Quando eu faço uma pergunta ao sistema, ele me retorna em valor booleano.
+print("Nome:", nome, "Idade:", idade, "É maior de idade:", maior_de_idade)
