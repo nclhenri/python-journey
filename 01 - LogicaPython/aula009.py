24dd6e2de521ed12aa4cf86d12c2accb3eeb7726
nclhenri <nicolashenrisilva@outlook.com>
2026-03-02 19:38:32 -0300

    Introdução a operadores matemáticos

---
+++ 01 - LogicaPython/aula009.py
@@ -0,0 +1,26 @@
+adicao = 10 + 10
+print("Adição:", adicao)
+
+subtracao = 10 - 5
+print("Subtração:", subtracao)
+
+multiplicacao = 10 * 10
+print("Multiplicação:", multiplicacao)
+
+divisao = 10 / 2.2 # Resultado em Float
+print("Divisão:", divisao)
+
+divisao_inteira = 10 // 2.2
+"""
+Caso eu queira que a divisão retorne um número inteiro
+será necessário fazer o casting.
+divisao_inteira = int(10 // 2.2)
+Resultado: 4
+"""
+print("Divisão Inteira:", divisao_inteira)
+
+exponenciacao = 2 ** 10
+print("Exponenciação:", exponenciacao)
+
+mod = 55 % 2
+print("Resto:", mod)
