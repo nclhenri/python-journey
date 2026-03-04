8ce4846a7e6d9fc106c4eab196bc648a49d4d276
nclhenri <nicolashenrisilva@outlook.com>
2026-03-04 19:51:26 -0300

    Operadores Relacionais

---
+++ 01 - LogicaPython/aula019.py
@@ -0,0 +1,19 @@
+maior = 5 > 2
+# Maior
+
+maior_ou_igual = 5 >= 5
+# Maior ou igual
+
+menor = 5 < 10
+# Menor
+
+menor_ou_igual = 5 <= 5
+# Menor ou igual
+
+igual = 5 == 5
+# Igual
+
+diferente = 5 != 4
+# Diferente
+
+print(diferente)
