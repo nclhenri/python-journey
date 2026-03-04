21363df325ea44348aa6b09be4e935412b52abaa
nclhenri <nicolashenrisilva@outlook.com>
2026-03-03 21:27:01 -0300

    Estudos sobre IF ELIF ELSE

---
+++ 01 - LogicaPython/aula016.py
@@ -0,0 +1,8 @@
+pergunta = input("Você quer 'entrar' ou 'sair'? ")
+
+if pergunta == "entrar":
+    print("Você entrou no sistema!")
+elif pergunta == "sair":
+    print("Você saiu do sistema!")
+else:
+    print("Digite um valor válido!")
