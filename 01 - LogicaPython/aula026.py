628c107c69e1cb7e0f148d0a9b7d2b697367ee62
nclhenri <nicolashenrisilva@outlook.com>
2026-03-05 12:34:22 -0300

    Complemento sobre f-strings

---
+++ 01 - LogicaPython/aula026.py
@@ -0,0 +1,8 @@
+variavel = "abc"
+print(f"{variavel: >10}")
+# imprimindo espaços vazios a esquerda
+print(f"{variavel: <10}")
+# imprimindo espaços vazios a direita
+print(f"{variavel: ^10}")
+# imprimindo espaços vazios ao centro
+print(f"O hexadecimal de 1500 é {1500:08X}")
