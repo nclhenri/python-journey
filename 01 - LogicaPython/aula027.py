e97103ab89012a4c7ad823bad3c375ade115223c
nclhenri <nicolashenrisilva@outlook.com>
2026-03-05 17:49:29 -0300

    Fatiamento de String

---
+++ 01 - LogicaPython/aula027.py
@@ -0,0 +1,9 @@
+nome = "Nicolas"
+print(nome[4:7])
+print(nome[0:4])
+print(nome[0:])
+print(nome[:7])
+print(len(nome))
+print(nome[0:len(nome)])
+print(nome[0:7:2])
+print(nome[::-1]) # AO CONTRÁRIO!!! :0 :0 :0 \o/
