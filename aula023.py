3e1af30c5a89c0c4d65c13de53229a49a9529aa4
nclhenri <nicolashenrisilva@outlook.com>
2026-03-05 11:34:22 -0300

    Operador lógico not

---
+++ aula023.py
@@ -0,0 +1,16 @@
+senha = input("Digite a sua senha: ")
+
+if not senha:
+    print("Você não digitou nada")
+
+print(not 1)
+# False
+
+print(not 0)
+# True
+
+print(not True)
+# False
+
+print(not False)
+# True
