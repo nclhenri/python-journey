49f6512a04d2d7e039f45c8ddf57c9dcfad6aac9
nclhenri <nicolashenrisilva@outlook.com>
2026-03-10 16:42:52 -0300

    Operadores de atribuição com operadores aritméticos

---
+++ aula036.py
@@ -0,0 +1,9 @@
+contador = 0
+
+contador += 1 # Nova forma de incrementar valor (Valor antigo mais o valor novo)
+"""
+Serve também para os outros operadores aritméticos:
++= -= *= /= //= **= %=
+"""
+
+print(contador)
