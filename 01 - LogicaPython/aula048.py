0a32e6de9543ae26f91c35fe4f44b115c1f02b11
nclhenri <nicolashenrisilva@outlook.com>
2026-03-19 15:00:26 -0300

    Comandos listas

--- 01 - LogicaPython/aula048.py
+++ 01 - LogicaPython/aula048.py
@@ -1,6 +1,20 @@
-lista = ["Nicolas", 22, 2.2, True]
+lista = ["Nicolas", 22, 2.2, True] # Criar
+print("Essa é a lista antes da alteração:")
 print(lista)
-print(lista[0])
-lista[0] = "Henrique"
-print(lista[0])
+print(lista) # Ler
+lista[0] = "Henrique" # Alterar
+del lista[0] # Apagar 
+lista.append(20) # Adiciona o valor ao final da lista (último índice)
+lista.pop() # Remove o último item da lista ou uma valor especifico no parametro
+# del lista[-1] # Remove o último item da lista, independente da quantidade
+# lista.clear() # Limpa a lista por completo
+lista.insert(0, 5) # Inserir no indice 0 o valor 5
+print("Essa é a lista depois da alteração:")
 print(lista)
+"""
+Assim que eu apagar o indice 0, o indice 1 assume o 0 de forma imediata
+Ou seja: 
+0          1   2
+Henrique, 22, 2.2
+Assim que o Henrique for apagado, o valor "22", assumirá o indice 0
+"""
