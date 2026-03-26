6fc74d4422e4996c415e2510fce17cc1d0b3442d
nclhenri <nicolashenrisilva@outlook.com>
2026-03-26 13:05:45 -0300

    argumentos nomeados e nao nomeados

---
+++ 02 - PythonIntermediario/aula066.py
@@ -0,0 +1,5 @@
+def soma(x, y):
+    print(f"A soma de {x} e {y} é de: {x + y}")
+
+soma(1, 2) # Isso são argumentos não nomeados
+soma (x=2, y=1) # Isso são argumentos nomeados (inverti os valores)
