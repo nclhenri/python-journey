d8a9a3d9b7eb0205bac7e6baffa4c0a48a23f19a
nclhenri <nicolashenrisilva@outlook.com>
2026-04-03 15:58:25 -0300

    Aula sobre return

---
+++ 02 - PythonIntermediario/aula070.py
@@ -0,0 +1,9 @@
+"""
+Retorno de valores das funções (return)
+"""
+
+def soma (x , y):
+    return x + y # A sua função retornar algo e você poderá utilziar esse valor
+
+soma1 = soma(2 , 2)
+print(soma1)
