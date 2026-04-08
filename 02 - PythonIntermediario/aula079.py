58f06aba1d04933e7d25cfd8b0096380fa6cca40
nclhenri <nicolashenrisilva@outlook.com>
2026-04-08 20:14:07 -0300

    Peculiaridades do tipo mutável set python

---
+++ 02 - PythonIntermediario/aula079.py
@@ -0,0 +1,12 @@
+# s1 = set("Nicolas")
+s1 = {"Nicolas", 4}
+print(s1)
+
+
+# Seus valores são únicos:
+
+s2 = {1,2, 3, 3, 3, 3, 3, 1}
+print(s2) # 1, 2, 3
+
+for i, elemento in enumerate(s1):
+    print(i, elemento)
